80becc6431ed015a51e7056302483339f57dc7a0
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-14 20:21:45 +0000

    tidying up main slightly

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ app = FastAPI(title="dbasik - datamaps for the web",
               description='Stop using spreadsheets to store data. Extract with dbasik.',
               version='1.0')
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(project.router)
-app.include_router(home.router)
-app.include_router(account.router)
+
+def configure():
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.include_router(project.router)
+    app.include_router(home.router)
+    app.include_router(account.router)
+
+
+def main():
+    configure()
+    uvicorn.run(app)
+
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    main()
