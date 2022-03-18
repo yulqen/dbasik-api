4e49bcdad4f1ae8e081185029e64a7bb412e3b32
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-18 16:19:11 +0000

    wip: fixed major projects list on index

--- services/project_service.py
+++ services/project_service.py
@@ -21,22 +21,8 @@ def get_name_by_id(project_id: int):
 
 
 def major_projects(limit: int = 2) -> list[dict[str, str | int]]:
-    return [
-               {
-                   "budget": "20M",
-                   "type": "Rubbish",
-                   "name": "Railway Reclaimation Project",
-                   "id": 1,
-               },
-               {"budget": "40M", "type": "Useless", "name": "Stanley Banks Head Scheme", "id": 2},
-               {
-                   "budget": "50M",
-                   "type": "Trumpets",
-                   "name": "Rocking Robots Closure Meak",
-                   "id": 3,
-               },
-               {"budget": "200M", "type": "Rollocks", "name": "Bomba Clifton Hedges", "id": 4},
-           ][:limit]
+    session = db_session.create_session()
+    return session.query(Project).all()[:limit]
 
 
 def project_count() -> int:
