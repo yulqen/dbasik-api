f11d333657041df6f5822cb2cbfea6a0f4196925
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-20 17:56:47 +0000

    wip: very basic database test

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,8 @@ from starlette.testclient import TestClient
 
 from web.data.project import ProjectType, ProjectStage, Project, Tier
 
+from web.data.db_session import global_init
+
 from web.views import home
 from web.data.modelbase import SqlAlchemyBase
 from server import app
@@ -19,6 +21,14 @@ SqlAlchemyBase.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
+def override_global_init():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()  #type: ignore
+
+app.dependency_overrides[global_init] = override_global_init
 
 def test_index():
     project_type = ProjectType(name="Boring Project", description="Bollocks")
@@ -35,3 +45,22 @@ def test_index():
     session.commit()
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_project():
+    project_type = ProjectType(name="Boring Project", description="Bollocks")
+    project_stage = ProjectStage(name="Stage 1", description="Russles")
+    tier = Tier(name="Tier 1", description="This is a test Tier")
+    project = Project(
+        name=f"Test Project",
+        project_stage=project_stage,
+        project_type=project_type,
+        tier=tier,
+    )
+    session = TestingSessionLocal()
+    session.add_all([project_stage, project_type, tier, project])
+    session.commit()
+    p = session.query(Project).first()
+    assert p.name == 'Test Project'
+    assert p.project_type.name == 'Boring Project'
+
