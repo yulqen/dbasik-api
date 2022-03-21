f64d1b9b056fd3147b3e6ec94e2579e05944ce9f
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-21 07:29:30 +0000

    added datamap service test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 from web.data.datamap import Datamap
+from web.services.datamap_service import get_datamaps
 
 
 def test_index(client):
@@ -16,5 +17,10 @@ def test_projects_list_view(client, projects):
 def test_datamap_in_db(session, datamaps):
     session = session()
     dm = session.query(Datamap).first()
-    # TODO: this doesn't test our API yet
+    # TODO: this doesn't test our API
     assert dm.name == "Test Datamap"
+
+
+def test_datamap_service(datamaps):
+    dms = get_datamaps()
+    assert dms[0].name == "Test Datamap"  # type: ignore
