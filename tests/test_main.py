92517515d7454f9a8235f60ed7cc227f763e7c54
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-25 21:15:04 +0000

    added test for fastapi.UploadFile test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
-from web.data.datamap import Datamap
+from fastapi.datastructures import UploadFile
+from web.data.datamap import Datamap, DatamapLine
 from web.services.datamap_service import (
     get_datamaps,
     import_csv_to_datamap,
     get_datamap_by_id,
     get_datamap_lines_for_datamap,
+    import_uploaded_csv_to_datamap,
 )
 
 
@@ -45,3 +47,12 @@ def test_import_csv_lines_to_datamap(dm_csv, datamaplines, datamap, session):
     session = session()
     dm = session.query(Datamap).first()
     assert dm.lines[-1].key == "Test Key from CSV 2"
+
+
+def test_import_upload_datmap_csv(dm_csv, datamaplines, datamap, session):
+    f = open(dm_csv, "rb")
+    uf = UploadFile("toss.csv", f, "text/csv")
+    import_uploaded_csv_to_datamap(uf, datamap.name)
+    session = session()
+    dml = session.query(DatamapLine).first()
+    assert dml.key == "Test Key 1"
